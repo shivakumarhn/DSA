ca8c6a889cb8b8c446bbc6024eee31e3c0360de3
Shivakumar HN <shivakumarhn049@gmail.com>
2021-08-21 23:33:49 +0530

    Tower of Hanoi

---
+++ src/main/java/recursion/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class TowerOfHanoi {
+
+    //2 to power of n movements for given n
+    static void TOH(int n, char A, char B, char C) {
+
+        if (n == 1) {
+            System.out.println("Move 1 from " + A + " to " + C);
+            return;
+        }
+
+        TOH(n - 1, A, C, B);
+        System.out.println("Move " + n + " from " + A + " to " + C);
+        TOH(n - 1, B, A, C);
+    }
+
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        TOH(n, 'A', 'B', 'C');
+    }
+}
